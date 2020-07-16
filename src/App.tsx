08db49ecd5126b12068bca2718ebd20d27842de1
Romain KOENIG <romain.koenig@gmail.com>
2020-07-16 16:15:09 +0200

    project re-init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Game night</h1>
+      <p>This is just a placeholder...</p>
+    </>
+  );
+}
+
+export default App;
